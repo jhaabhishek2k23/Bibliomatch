1179871f647d45438665ec522bb663c06f7da203
jhaabhishek2k23 <jha.abhishek2k23@gmail.com>
2025-04-13 02:00:16 +0530

    initial setup for the home page is done with spring security enabled

--- src/main/java/com/backend/bibliomatch/controllers/WelcomeController.java
+++ src/main/java/com/backend/bibliomatch/controllers/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
     
     @GetMapping("/")
     public String getMethodName() {
-        return "index"; // Refers to templates/index.html
+        return "index"; 
     }
 }
